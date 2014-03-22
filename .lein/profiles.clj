0506c92c841819ed01241bbfbe34144da9534fcd
Martin Trojer <martin.trojer@gmail.com>
2014-03-22 15:24:31 +0000

    lein-try

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -3,6 +3,7 @@
                   [lein-cljsbuild "0.3.3"]
                   [lein-kibit "0.0.8"]
                   [lein-release "1.0.5"]
+                  [lein-try "0.4.1"]
                   [lein-vanity "0.2.0"]
                   [org.clojars.kurtharriger/lein-depgraph "0.1.0"]
                   [lein-pprint "1.1.1"]]
