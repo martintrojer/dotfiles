23bbdb832ce803425ebd6bd2a065c5c2bede0693
Martin Trojer <martin.trojer@gmail.com>
2014-04-01 18:21:55 +0100

    lein-cloverage

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,7 @@
 {:user {:plugins [[jonase/eastwood "0.1.0"]
                   [lein-ancient "0.5.4"]
                   [lein-cljsbuild "0.3.3"]
+                  [lein-cloverage "1.0.2"]
                   [lein-kibit "0.0.8"]
                   [lein-release "1.0.5"]
                   [lein-try "0.4.1"]
