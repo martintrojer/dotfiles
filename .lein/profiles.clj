99dc69d00ec19d46b0e48fd5db81d7ab059e18e7
Martin Trojer <martin.trojer@gmail.com>
2015-04-28 07:28:42 +0100

    trimming lein profile

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,11 +2,8 @@
                   [refactor-nrepl "0.2.2"]
                   [lein-ancient "0.6.2"]
                   [lein-cloverage "1.0.2"]
-                  [com.jakemccrary/lein-test-refresh "0.6.0"]
-                  [lein-release "1.1.3"]
                   [lein-try "0.4.3"]]
-        :dependencies [[clojure-complete "0.2.4"]
-                       [criterium "0.4.3"]
+        :dependencies [[criterium "0.4.3"]
                        [pjstadig/humane-test-output "0.6.0"]]
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]}}
