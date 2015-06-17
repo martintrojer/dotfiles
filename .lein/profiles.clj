0f74cb9bd7a10a15755f377e7ff22dc043733cb9
Martin Trojer <martin.trojer@gmail.com>
2015-06-17 08:37:41 +0100

    lein profile updates

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,9 +1,9 @@
-{:user {:plugins [[cider/cider-nrepl "0.8.2"]
-                  [refactor-nrepl "1.0.4"]
-                  [lein-ancient "0.6.7"]
-                  [lein-cloverage "1.0.3"]
+{:user {:plugins [[cider/cider-nrepl "0.9.0"]
+                  [refactor-nrepl "1.0.5"]
+                  [lein-ancient "0.6.7" :exclusions [org.clojure/clojure commons-codec commons-logging]]
                   [lein-try "0.4.3"]]
         :dependencies [[criterium "0.4.3"]
+                       [org.clojure/tools.nrepl "0.2.10"]
                        [pjstadig/humane-test-output "0.7.0"]]
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]}}
