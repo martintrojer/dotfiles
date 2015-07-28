47986d0fe332080f2fd558eb32249086929148e6
Martin Trojer <martin.trojer@gmail.com>
2015-07-28 08:52:01 +0100

    new clj-refactor middleware

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,5 @@
 {:user {:plugins [[cider/cider-nrepl "0.9.1"]
-                  [refactor-nrepl "1.0.5"]
+                  [refactor-nrepl "1.1.0"]
                   [lein-ancient "0.6.7" :exclusions [org.clojure/clojure commons-codec commons-logging]]
                   [lein-try "0.4.3"]]
         :dependencies [[criterium "0.4.3"]
