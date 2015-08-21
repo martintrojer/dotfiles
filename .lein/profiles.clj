e02c5e7b404a466a4f5bd907e65d9ab14816146b
Martin Trojer <martin.trojer@gmail.com>
2015-08-21 16:43:40 +0100

    eastwood is handy sometimes

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,6 @@
 {:user {:plugins [[cider/cider-nrepl "0.9.1"]
                   [refactor-nrepl "1.1.0"]
-                  [lein-ancient "0.6.7" :exclusions [org.clojure/clojure commons-codec commons-logging]]
-                  [lein-try "0.4.3"]]
+                  [jonase/eastwood "0.2.1"]
+                  [lein-ancient "0.6.7" :exclusions [org.clojure/clojure commons-codec commons-logging]]]
         :dependencies [[criterium "0.4.3"]
                        [org.clojure/tools.nrepl "0.2.10"]]}}
