948d88a6fa774df3ab7acd2f26755be12d20f2f8
Martin Trojer <martin.trojer@gmail.com>
2015-11-18 16:52:54 +0000

    faster lein startup

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,6 @@
-{:user {:plugins [[cider/cider-nrepl "0.9.1"]
-                  [refactor-nrepl "1.1.0"]
-                  [jonase/eastwood "0.2.1"]
-                  [lein-ancient "0.6.7" :exclusions [org.clojure/clojure commons-codec commons-logging]]]
+{:user {:plugins [[jonase/eastwood "0.2.1"]
+                  [lein-ancient "0.6.7" :exclusions [org.clojure/clojure commons-codec commons-logging]]]}
+ :repl {:plugins [[cider/cider-nrepl "0.9.1"]
+                  [refactor-nrepl "1.1.0"]]
         :dependencies [[criterium "0.4.3"]
                        [org.clojure/tools.nrepl "0.2.10"]]}}
