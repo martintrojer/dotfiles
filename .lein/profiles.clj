6e4346f70adc1ba9a8f1d93fbdf19e7842ef35c1
Martin Trojer <martin.trojer@gmail.com>
2015-12-13 08:32:34 +0000

    lein profile version bumps

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,6 @@
-{:user {:plugins [[jonase/eastwood "0.2.1"]
-                  [lein-ancient "0.6.7" :exclusions [org.clojure/clojure commons-codec commons-logging]]]}
- :repl {:plugins [[cider/cider-nrepl "0.9.1"]
-                  [refactor-nrepl "1.1.0"]]
+{:user {:plugins [[jonase/eastwood "0.2.2"]
+                  [lein-ancient "0.6.8" :exclusions [org.clojure/clojure commons-codec commons-logging]]]}
+ :repl {:plugins [[cider/cider-nrepl "0.10.0"]
+                  [refactor-nrepl "2.0.0-SNAPSHOT" :exclusions [org.clojure/clojure]]]
         :dependencies [[criterium "0.4.3"]
-                       [org.clojure/tools.nrepl "0.2.10"]]}}
+                       [org.clojure/tools.nrepl "0.2.12"]]}}
