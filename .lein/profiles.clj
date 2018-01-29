8b8780c3fc8063b720ae475f3248dd1797ce3dce
Martin Trojer <martin.trojer@gmail.com>
2018-01-29 18:37:04 +0000

    lein

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,6 @@
 {:user {:plugins [[jonase/eastwood "0.2.3"]
                   [lein-ancient "0.6.10" :exclusions [org.clojure/clojure commons-codec commons-logging]]]}
- :repl {:plugins [[cider/cider-nrepl "0.13.0"]
+ :repl {:plugins [[cider/cider-nrepl "0.14.0"]
                   [refactor-nrepl "2.2.0" :exclusions [org.clojure/clojure]]]
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]
                        [criterium "0.4.4"]]}}
