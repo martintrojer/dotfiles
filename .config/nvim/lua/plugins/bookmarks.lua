373f876663a6c7dcff0c253032d4d361845dd8fd
Martin Trojer <mtrojer@fb.com>
2024-03-05 15:26:53 +0000

    neovim tweaks

--- .config/nvim/lua/plugins/bookmarks.lua
+++ .config/nvim/lua/plugins/bookmarks.lua
@@ -14,12 +14,12 @@ return { -- Bookmarking important files.
 			on_attach = function()
 				local bm = require("bookmarks")
 				local map = vim.keymap.set
-				map("n", "mm", bm.bookmark_toggle) -- add or remove bookmark at current line
-				map("n", "mi", bm.bookmark_ann) -- add or edit mark annotation at current line
-				map("n", "mc", bm.bookmark_clean) -- clean all marks in local buffer
-				map("n", "mn", bm.bookmark_next) -- jump to next mark in local buffer
-				map("n", "mp", bm.bookmark_prev) -- jump to previous mark in local buffer
-				map("n", "ml", bm.bookmark_list) -- show marked file list in quickfix window
+				map("n", "mm", bm.bookmark_toggle, { desc = "[b]ookmark [t]oggle" }) -- add or remove bookmark at current line
+				map("n", "mi", bm.bookmark_ann, { desc = "[b]ookmark add/edit" }) -- add or edit mark annotation at current line
+				map("n", "mc", bm.bookmark_clean, { desc = "[b]ookmark [c]lean (current file)" }) -- clean all marks in local buffer
+				map("n", "mn", bm.bookmark_next, { desc = "[b]ookmark jump [n]ext" }) -- jump to next mark in local buffer
+				map("n", "mp", bm.bookmark_prev, { desc = "[b]ookmark jump [p]rev" }) -- jump to previous mark in local buffer
+				map("n", "ml", bm.bookmark_list, { desc = "[b]ookmark [l]ist" }) -- show marked file list in quickfix window
 			end,
 		})
 	end,
