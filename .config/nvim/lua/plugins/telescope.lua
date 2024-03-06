96959d52b72f43010b235f3d6dc6cbbddabbfc3b
Martin Trojer <mtrojer@meta.com>
2024-03-06 07:41:20 -0800

    bookmarks

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -29,7 +29,8 @@ return { -- Fuzzy Finder (files, lsp, etc)
 
 		-- [[ Configure Telescope ]]
 		-- See `:help telescope` and `:help telescope.setup()`
-		require("telescope").setup({
+		local telescope = require("telescope")
+		telescope.setup({
 			defaults = {
 				mappings = {
 					i = { ["<c-enter>"] = "to_fuzzy_refine" },
@@ -44,11 +45,11 @@ return { -- Fuzzy Finder (files, lsp, etc)
 		})
 
 		-- Enable telescope extensions, if they are installed
-		pcall(require("telescope").load_extension, "fzf")
-		pcall(require("telescope").load_extension, "ui-select")
-		require("telescope").load_extension("live_grep_args")
-		require("telescope").load_extension("file_browser")
-		require("telescope").load_extension("bookmarks")
+		pcall(telescope.load_extension, "fzf")
+		pcall(telescope.load_extension, "ui-select")
+		telescope.load_extension("live_grep_args")
+		telescope.load_extension("file_browser")
+		telescope.load_extension("bookmarks")
 		-- See `:help telescope.builtin`
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
@@ -56,6 +57,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
 		vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
 		vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
 		vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+		vim.keymap.set("n", "<leader>sb", telescope.extensions.bookmarks.list, { desc = "[S]earch [B]ookmarks" })
 		vim.keymap.set(
 			"n",
 			"<leader>sg",
