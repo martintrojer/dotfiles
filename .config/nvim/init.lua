aae8306096215597001f4d20f73967a6cd375648
Martin Trojer <martin.trojer@gmail.com>
2024-03-10 18:18:38 +0000

    expandtab

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -58,7 +58,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+vim.opt.expandtab = true
 vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 
 vim.opt.spell = true
 vim.opt.spelllang = "en_us"
