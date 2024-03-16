8253ca0821107dc1b3ca36626fc73922c0817e74
Martin Trojer <martin.trojer@gmail.com>
2024-03-16 16:43:56 +0000

    telescope git_grep

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -20,6 +20,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
 		{ "nvim-telescope/telescope-ui-select.nvim" },
 		{ "nvim-tree/nvim-web-devicons" },
 		{ "nvim-telescope/telescope-file-browser.nvim" },
+		{ "davvid/telescope-git-grep.nvim" },
 	},
 	config = function()
 		-- Two important keymaps to use while in telescope are:
@@ -46,8 +47,10 @@ return { -- Fuzzy Finder (files, lsp, etc)
 		-- Enable telescope extensions, if they are installed
 		pcall(telescope.load_extension, "fzf")
 		pcall(telescope.load_extension, "ui-select")
-		telescope.load_extension("file_browser")
-		telescope.load_extension("bookmarks")
+		pcall(telescope.load_extension("file_browser"))
+		pcall(telescope.load_extension("bookmarks"))
+		pcall(telescope.load_extension("git_grep"))
+
 		-- See `:help telescope.builtin`
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
@@ -55,8 +58,9 @@ return { -- Fuzzy Finder (files, lsp, etc)
 		vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
 		vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
 		vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-		vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
 		vim.keymap.set("n", "<leader>sb", telescope.extensions.bookmarks.list, { desc = "[S]earch [B]ookmarks" })
+		vim.keymap.set("n", "<leader>sg", telescope.extensions.git_grep.grep, { desc = "[S]earch by [G]it grep" })
+		vim.keymap.set("n", "<leader>sp", builtin.live_grep, { desc = "[S]earch by [G]rep" })
 		vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
 		vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
 		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
