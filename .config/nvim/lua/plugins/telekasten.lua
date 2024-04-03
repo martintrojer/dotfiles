9cb311ef93028d57e5bc6e640ab72cb4322e57c8
Martin Trojer <martin.trojer@gmail.com>
2024-04-03 10:34:24 +0100

    better shortcuts

--- .config/nvim/lua/plugins/telekasten.lua
+++ .config/nvim/lua/plugins/telekasten.lua
@@ -32,11 +32,12 @@ return {
 		vim.keymap.set("n", "<leader>zf", "<cmd>Telekasten find_notes<CR>")
 		vim.keymap.set("n", "<leader>zg", "<cmd>Telekasten search_notes<CR>")
 		vim.keymap.set("n", "<leader>zd", "<cmd>Telekasten goto_today<CR>")
-		vim.keymap.set("n", "<leader>zz", "<cmd>Telekasten follow_link<CR>")
+		vim.keymap.set("n", "<leader>zo", "<cmd>Telekasten follow_link<CR>")
 		vim.keymap.set("n", "<leader>zn", "<cmd>Telekasten new_note<CR>")
 		vim.keymap.set("n", "<leader>zc", "<cmd>Telekasten show_calendar<CR>")
 		vim.keymap.set("n", "<leader>zb", "<cmd>Telekasten show_backlinks<CR>")
-		vim.keymap.set("n", "<leader>zI", "<cmd>Telekasten insert_img_link<CR>")
+		vim.keymap.set("n", "<leader>zt", "<cmd>Telekasten toggle_todo <CR>")
+		vim.keymap.set("n", "<leader>zz", "<cmd>Telekasten show_tags <CR>")
 
 		-- Call insert link automatically when we start typing a link
 		vim.keymap.set("i", "[[", "<cmd>Telekasten insert_link<CR>")
