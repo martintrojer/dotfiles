3879427e91f636f3ada7587af6b1b9c4e8fce35c
Martin Trojer <mtrojer@fb.com>
2024-04-09 09:41:12 +0100

    ranger > neotree

--- .config/nvim/lua/plugins/floatterm.lua
+++ .config/nvim/lua/plugins/floatterm.lua
@@ -21,6 +21,8 @@ return {
 		vim.keymap.set("n", "<F12>", "<cmd>FloatermToggle<CR>")
 		vim.keymap.set("t", "<F12>", "<C-\\><C-n><cmd>FloatermToggle<CR>")
 
+		vim.keymap.set("n", "<leader>o", "<cmd>FloatermNew ranger %:h<CR>", { desc = "Open Ranger" })
+
 		vim.cmd("autocmd! TermOpen term://* lua ft_set_terminal_keymaps()")
 	end,
 }
