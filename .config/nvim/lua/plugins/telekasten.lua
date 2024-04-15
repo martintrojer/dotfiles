e1d8af07f1889393de8b8484d6b0ccb1d9b3bdf2
Martin Trojer <mtrojer@fb.com>
2024-04-15 10:36:59 +0100

    better date insert

--- .config/nvim/lua/plugins/telekasten.lua
+++ .config/nvim/lua/plugins/telekasten.lua
@@ -44,7 +44,7 @@ return {
 		vim.keymap.set("n", "<leader>zi", function()
 			local pos = vim.api.nvim_win_get_cursor(0)[2]
 			local line = vim.api.nvim_get_current_line()
-			local nline = line:sub(0, pos) .. os.date("%Y-%m-%d") .. line:sub(pos + 1)
+			local nline = line:sub(0, pos) .. "## " .. os.date("%Y-%m-%d") .. line:sub(pos + 1)
 			vim.api.nvim_set_current_line(nline)
 		end, { desc = "Insert current date" })
 	end,
