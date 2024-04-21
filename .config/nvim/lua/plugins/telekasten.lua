63c192b50cb8177fdb003e968fe885d32a9c5e7c
Martin Trojer <martin.trojer@gmail.com>
2024-04-21 18:27:31 +0100

    shortcut to insert flashcard

--- .config/nvim/lua/plugins/telekasten.lua
+++ .config/nvim/lua/plugins/telekasten.lua
@@ -36,10 +36,16 @@ return {
 		vim.keymap.set("n", "<leader>zd", "<cmd>Telekasten goto_today<CR>")
 		vim.keymap.set("n", "<leader>zo", "<cmd>Telekasten follow_link<CR>")
 		vim.keymap.set("n", "<leader>zn", "<cmd>Telekasten new_note<CR>")
-		vim.keymap.set("n", "<leader>zc", "<cmd>Telekasten show_calendar<CR>")
 		vim.keymap.set("n", "<leader>zb", "<cmd>Telekasten show_backlinks<CR>")
 		vim.keymap.set("n", "<leader>zt", "<cmd>Telekasten toggle_todo<CR>")
 		vim.keymap.set("n", "<leader>zz", "<cmd>Telekasten show_tags<CR>")
+
+		vim.keymap.set("n", "<leader>zc", function()
+			local pos = vim.api.nvim_win_get_cursor(0)[2]
+			local line = vim.api.nvim_get_current_line()
+			local nline = line:sub(0, pos) .. "<PROMPT> : <RESPONSE> 🧠 #tag" .. line:sub(pos + 1)
+			vim.api.nvim_set_current_line(nline)
+		end, { desc = "Insert flash card" })
 
 		vim.keymap.set("n", "<leader>zi", function()
 			local pos = vim.api.nvim_win_get_cursor(0)[2]
