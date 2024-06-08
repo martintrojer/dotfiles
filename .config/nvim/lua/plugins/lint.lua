ab7963163de555c206982f8ae5d769ac69d1dd88
Martin Trojer <martin.trojer@gmail.com>
2024-06-08 12:48:46 +0100

    lint

---
+++ .config/nvim/lua/plugins/lint.lua
@@ -0,0 +1,22 @@
+return {
+	"mfussenegger/nvim-lint",
+	events = { "BufReadPre", "BufNewfile" },
+	config = function()
+		local lint = require("lint")
+		lint.linters_by_ft = {
+			-- lua = { "luacheck" },
+			-- python = { "mypy", "flake8" },
+		}
+
+		local lint_ag = vim.api.nvim_create_augroup("lint", { clear = true })
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+			group = lint_ag,
+			callback = function()
+				lint.try_lint()
+			end,
+		})
+		vim.keymap.set("n", "<leader>l", function()
+			lint.try_lint()
+		end, { desc = "Lint of current file" })
+	end,
+}
