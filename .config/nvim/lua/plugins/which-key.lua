de2968ab11e85571c230ed58118751008c433d0c
Martin Trojer <martin.trojer@gmail.com>
2024-08-27 17:16:15 +0100

    update which-key configs

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -5,14 +5,14 @@ return {
 		config = function() -- This is the function that runs, AFTER loading
 			require("which-key").setup()
 			-- Document existing key chains
-			require("which-key").register({
-				["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-				["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-				["<leader>f"] = { name = "[F]ile Browser", _ = "which_key_ignore" },
-				["<leader>g"] = { name = "[G]it", _ = "which_key_ignore" },
-				["<leader>h"] = { name = "[H]arpoon", _ = "which_key_ignore" },
-				["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-				["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+			require("which-key").add({
+				{ "<leader>c", group = "[C]ode" },
+				{ "<leader>d", group = "[D]ocument" },
+				{ "<leader>f", group = "[F]ile Browser" },
+				{ "<leader>g", group = "[G]it" },
+				{ "<leader>h", group = "[H]arpoon" },
+				{ "<leader>s", group = "[S]earch" },
+				{ "<leader>w", group = "[W]orkspace" },
 			})
 		end,
 	},
