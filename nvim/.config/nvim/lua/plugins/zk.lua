c9b9c00a9e01cf4eda4de69017f634f8e548959b
Martin Trojer <martin.trojer@gmail.com>
2025-05-01 08:23:08 +0100

    chore: Add descriptions to zk keymaps for better discoverability
    CHANGES
    
    *   Add description to create new notes keymap.
    *   Add description to weekly journal keymap.
    *   Add description to find notes by modified date keymap.
    *   Add description to find notes matching selection keymap.
    *   Add description to find notes by tag keymap.
    *   Add description to search notes by regex keymap.
    *   Add description to list notes linked by current note.
    *   Add description to list notes linking to current note.

--- nvim/.config/nvim/lua/plugins/zk.lua
+++ nvim/.config/nvim/lua/plugins/zk.lua
@@ -7,21 +7,26 @@ return {
 			-- See Setup section below
 		})
 
-		vim.keymap.set("n", "<leader>zn", "<Cmd>ZkNew { title = vim.fn.input('Title: ') }<CR>", opts)
-		vim.keymap.set("n", "<leader>zw", "<Cmd>ZkNew { group = 'journal' }<CR>", opts)
-		vim.keymap.set("n", "<leader>zf", "<Cmd>ZkNotes { sort = { 'modified' } }<CR>", opts)
-		vim.keymap.set("v", "<leader>zf", ":'<,'>ZkMatch<CR>", opts)
-		vim.keymap.set("n", "<leader>zz", "<Cmd>ZkTags<CR>", opts)
+		vim.keymap.set("n", "<leader>zn", "<Cmd>ZkNew { title = vim.fn.input('Title: ') }<CR>", { desc = "New Note" })
+		vim.keymap.set(
+			"n",
+			"<leader>zw",
+			"<Cmd>ZkNew { group = 'journal' }<CR>",
+			{ desc = "Create/Edit Weekly Journal" }
+		)
+		vim.keymap.set("n", "<leader>zf", "<Cmd>ZkNotes { sort = { 'modified' } }<CR>", { desc = "Find Notes" })
+		vim.keymap.set("v", "<leader>zf", ":'<,'>ZkMatch<CR>", { desc = "Find Notes Matching Selection" })
+		vim.keymap.set("n", "<leader>zz", "<Cmd>ZkTags<CR>", { desc = "Find Notes by Tag" })
 
 		-- Search for the notes matching a given query.
 		vim.keymap.set(
 			"n",
 			"<leader>zs",
 			"<Cmd>ZkNotes { sort = { 'modified' }, match = { vim.fn.input('Search: ') } }<CR>",
-			opts
+			{ desc = "Search Notes Matching Regex" }
 		)
 
-		vim.keymap.set("n", "<leader>zl", "<Cmd>ZkLinks<CR>", opts)
-		vim.keymap.set("n", "<leader>zb", "<Cmd>ZkBacklinks<CR>", opts)
+		vim.keymap.set("n", "<leader>zl", "<Cmd>ZkLinks<CR>", { desc = "Notes Linked By Current Note" })
+		vim.keymap.set("n", "<leader>zb", "<Cmd>ZkBacklinks<CR>", { desc = "Notes Linking To Current Note" })
 	end,
 }
