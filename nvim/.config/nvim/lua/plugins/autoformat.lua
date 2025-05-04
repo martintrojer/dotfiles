19b9c7d1808c5b9a0d2a5adfc93bfca6bf21cd31
Martin Trojer <martin.trojer@gmail.com>
2025-05-04 14:27:59 +0100

    better json/css formatting

--- nvim/.config/nvim/lua/plugins/autoformat.lua
+++ nvim/.config/nvim/lua/plugins/autoformat.lua
@@ -8,9 +8,11 @@ return {
 			lsp_fallback = true,
 		},
 		formatters_by_ft = {
+			css = { "prettier" },
 			html = { "prettier" },
 			javascript = { "prettier" },
 			json = { "prettier" },
+			jsonc = { "prettier" },
 			lua = { "stylua" },
 			python = { "ruff_format", "ruff_fix" },
 			rust = { "rustfmt" },
