d260f0296b35c943de127ed26e3d6f18371e102e
Martin Trojer <martin.trojer@gmail.com>
2025-08-04 14:49:26 +0100

    more extras

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -11,27 +11,19 @@ return {
       "bash-language-server", -- bashls
       "codelldb",
       "eslint-lsp", -- eslint
-      "gopls",
       "graphql-language-service-cli", -- graphql
-      "haskell-language-server", -- hls
-      "hlint",
       "jsonlint",
       "lua-language-server", -- lua_ls
       "luacheck",
-      "markdownlint",
       "prettier",
       "proselint",
-      "pyright",
-      "ruff",
       "rust-analyzer", -- rust_analyzer
       "shellcheck",
       "sqlfluff",
-      "stylua",
-      "tree-sitter-cli",
       "typescript-language-server", -- ts_ls
       "typos-lsp", -- typos_lsp
-      "vale",
       "write-good",
+      "zk",
     }
     require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
   end,
