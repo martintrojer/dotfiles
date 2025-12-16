07c12e52b520a1e086bc6e17031a82528d97cc26
Martin Trojer <martin.trojer@gmail.com>
2025-12-16 21:10:16 +0000

    move lock to menu

--- waybar/.config/waybar/scripts/powermenu.sh
+++ waybar/.config/waybar/scripts/powermenu.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-chosen=$(echo -e "󰤄 Suspend\n󰐥 Shutdown\n󰜺 Reboot\n󰗽 Logout" | wofi --dmenu --width 200 --height 250 --prompt "Power Menu")
+chosen=$(echo -e "󰌾 Lock\n󰜺 Reboot\n󰗽 Logout\n󰐥 Shutdown\n󰤄 Suspend" | wofi --dmenu -i -O default --width 250 --height 300 --prompt "Power Menu")
 
 case "$chosen" in
+    *Lock*)
+        if command -v hyprlock &> /dev/null; then
+            hyprlock
+        elif command -v niri &> /dev/null; then
+            niri msg action lock
+        fi
+        ;;
     *Suspend*)
         systemctl suspend
         ;;
