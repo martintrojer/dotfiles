b6511bee2822e8b46cd6c8ff9e2da3eaaad09066
Martin Trojer <martin.trojer@gmail.com>
2026-01-02 15:17:41 +0000

    fedora setup

---
+++ fedora/setup-toolbox.sh
@@ -0,0 +1,11 @@
+dnf copr enable lihaohong/yazi
+dnf copr enable terjeros/eza
+dnf copr enable atim/starship
+
+dnf install \
+  vim neovim \
+  zsh \
+  htop btop \
+  gdu fd-find ripgrep yazi \
+  fzf zoxide tmux bat eza starship \
+  nodejs
