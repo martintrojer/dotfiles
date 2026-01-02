4ed4e5d642f1b9580619e9477d0cef0f5ffb4888
Martin Trojer <martin.trojer@gmail.com>
2026-01-02 15:17:41 +0000

    fedora setup

---
+++ fedora/setup-base.sh
@@ -0,0 +1,16 @@
+# https://copr.fedorainfracloud.org/coprs/lihaohong/yazi/
+# https://copr.fedorainfracloud.org/coprs/terjeros/eza
+# https://copr.fedorainfracloud.org/coprs/atim/starship
+
+# Download the .repo file. there is a download link for YOUR release
+# Copy to /etc/yum.repos.d:
+
+# rpm-ostree refresh-md
+
+rpm-ostree install \
+  vim neovim \
+  zsh \
+  htop btop \
+  stow rclone gdu fd-find ripgrep 7zip yazi \
+  fzf zoxide tmux bat eza starship \
+  distrobox
---
+++ fedora/setup-niri.sh
@@ -0,0 +1,13 @@
+# https://copr.fedorainfracloud.org/coprs/acidburnmonkey/hyprland/
+# https://copr.fedorainfracloud.org/coprs/scottames/ghostty/
+
+rpm-ostree install \
+  niri \
+  waybar \
+  hyprpaper hyprlock hypridle \
+  ghostty xdg-terminal-exec \
+  wofi \
+  playerctl wiremix \
+  mako \
+  fontawesome-fonts-all \
+  fastfetch
