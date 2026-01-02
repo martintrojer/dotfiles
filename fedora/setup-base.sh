bb7fbb869a5bd226aeb76cc052009b25a15da7f0
Martin Trojer <martin.trojer@gmail.com>
2026-01-02 17:37:47 +0000

    Remove grubby from the package installation list in setup-base.sh

--- fedora/setup-base.sh
+++ fedora/setup-base.sh
@@ -10,7 +10,7 @@
 rpm-ostree install \
   vim neovim \
   zsh \
-  htop btop grubby \
+  htop btop \
   stow rclone gdu fd-find ripgrep 7zip yazi \
   fzf zoxide tmux bat eza starship \
   distrobox
