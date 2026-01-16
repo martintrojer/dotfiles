92fccc8ba28ff77098f0bb2b66cb1d0009662bd1
Martin Trojer <martin.trojer@gmail.com>
2026-01-16 08:20:12 +0000

    Add OSC52 clipboard support to neovim, tmux and ghostty
    Enables copying via OSC52 escape sequences, which works over SSH
    and in terminals that support it without requiring external tools.

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,7 +1,17 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
---
--- Example:
--- vim.opt.number = true
--- vim.opt.relativenumber = true
+
+-- Use OSC52 for clipboard (works over SSH and in terminals that support it)
+vim.opt.clipboard = "unnamedplus"
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
