b76a0eb307a9725fc2ac82d045e35a4ad2f5cd13
Martin Trojer <martin.trojer@gmail.com>
2026-01-23 19:15:45 +0000

    rpi

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -1,5 +1,6 @@
 # General aliases
 alias -g F='| fzf'
+alias nvim=vim
 alias port_forward='ssh -L 8081:localhost:8081 dev'
 alias serve='python3 -m http.server 8081'
 test -e "/opt/homebrew/bin/gdu-go" && alias gdu='gdu-go'
