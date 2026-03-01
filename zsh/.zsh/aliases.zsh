68e4d62d76bf152f2c8aeefff6e1185ffbdade08
Martin Trojer <martin.trojer@gmail.com>
2026-03-01 18:50:42 +0000

    ollama on fedora

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -5,9 +5,11 @@ alias serve='python3 -m http.server 8081'
 test -e "/opt/homebrew/bin/gdu-go" && alias gdu='gdu-go'
 
 # Common `less` misspellings
-alias les='less'
-alias lss='less'
-alias lesss='less'
-alias ess='less'
 alias lees='less'
+alias elss='less'
 alias sless='less'
+
+# Only alias ollama to toolbox if it's not installed on the host
+if ! command -v ollama >/dev/null 2>&1; then
+    alias ollama='toolbox run -c ollama ollama'
+fi
