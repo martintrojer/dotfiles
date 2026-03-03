2c2a9884c95998afa7295f84e6542cbf7d382e64
Martin Trojer <martin.trojer@gmail.com>
2026-03-03 18:00:19 +0000

    improve ollama alias

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -9,7 +9,15 @@ alias lees='less'
 alias elss='less'
 alias sless='less'
 
-# Only alias ollama to toolbox if it's not installed on the host
-if ! command -v ollama >/dev/null 2>&1; then
-    alias ollama='toolbox run -c ollama ollama'
+# Only alias ollama to toolbox on Fedora-family distros if it's not installed on the host
+if [[ -r /etc/os-release ]]; then
+    # shellcheck disable=SC1091
+    . /etc/os-release
+
+    distro_id_lc="${ID:l}"
+    distro_like_lc="${ID_LIKE:l}"
+    if [[ "$distro_id_lc" == *fedora* || "$distro_like_lc" == *fedora* || "$distro_like_lc" == *rhel* ]] \
+        && ! command -v ollama >/dev/null 2>&1; then
+        alias ollama='toolbox run -c ollama ollama'
+    fi
 fi
